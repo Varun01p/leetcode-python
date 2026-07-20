41b9d08539613437a666a3a42ee664f1f690a8c6
Varun Pednekar~ <varunnn47@gmail.com>
2026-07-20 21:36:13 +0530

    Add LeetCode 9, 217 and 242 solutions

---
+++ Two_pointers/Merge_sorted_array.py
@@ -0,0 +1,36 @@
+def merge(nums1, m, nums2, n):
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
+
+    while p1 >= 0 and p2 >= 0:
+        if nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+
+    while p2 >= 0:
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
+
+    return nums1
+
+
+# Test Case 1
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+print(merge(nums1, 3, nums2, 3))
+
+# Test Case 2
+nums1 = [1]
+nums2 = []
+print(merge(nums1, 1, nums2, 0))
+
+# Test Case 3
+nums1 = [0]
+nums2 = [1]
+print(merge(nums1, 0, nums2, 1))
